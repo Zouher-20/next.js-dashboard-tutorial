45b1cc70a3c5a136ef47b209db67a3d558d6df42
zouhairnasser <zouhairnasser@warshatec.com>
2023-10-28 22:50:15 +0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+import { Play } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const play = Inter({ subsets: ["latin"] });
